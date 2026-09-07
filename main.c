7a859f421bda9551bb73664c531f12cf428cf952
Parking Offender <143820709+Finn-bit27@users.noreply.github.com>
2026-09-07 15:29:17 -0400

    Implement pigpio initialization and termination
    Added pigpio initialization and termination calls.

--- main.c
+++ main.c
@@ -1,2 +1,13 @@
 //orchestrator
 #import <stdio.h>
+
+gpioInitialise(void);
+
+if (gpioInitialise() < 0){
+   // pigpio initialisation failed.
+} else{
+   // pigpio initialised okay.
+}
+
+
+gpioTerminate(void);
